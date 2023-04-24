99586fe862e074c3792dc65dcc9b3585cab1d11b
remo <tarnay.marton@gmail.com>
2023-04-24 17:40:57 +0200

    fix tests

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -17,30 +17,30 @@ public class TrainSensorTest {
     public void before() {
         mockTU = mock(TrainUser.class);
         mockTC = mock(TrainController.class);
-        sensor = new TrainSensor(mockTC, mockTU);
+        sensor = new TrainSensorImpl(mockTC, mockTU);
     }
 
     @Test
     public void setSpeedLimitNoAlarm() {
         when(mockTC.getReferenceSpeed()).thenReturn(50);
-        when(mockTC.setAlarmState(true)).thenReturn(void);
-        sensor.setSpeedLimit(55);
+        doNothing().when(mockTC).setAlarmState(true);
+        sensor.overrideSpeedLimit(55);
         verify(mockTC, times(0)).setAlarmState(true);
     }
 
     @Test
     public void setSpeedLimitNegativeAlarm() {
         when(mockTC.getReferenceSpeed()).thenReturn(50);
-        when(mockTC.setAlarmState(true)).thenReturn(void);
-        sensor.setSpeedLimit(-1);
+        doNothing().when(mockTC).setAlarmState(true);
+        sensor.overrideSpeedLimit(-1);
         verify(mockTC, times(1)).setAlarmState(true);
     }
 
     @Test
     public void setSpeedLimitTooHighAlarm() {
         when(mockTC.getReferenceSpeed()).thenReturn(50);
-        when(mockTC.setAlarmState(true)).thenReturn(void);
-        sensor.setSpeedLimit(600);
+        doNothing().when(mockTC).setAlarmState(true);
+        sensor.overrideSpeedLimit(600);
         verify(mockTC, times(1)).setAlarmState(true);
     }
 
@@ -48,8 +48,8 @@ public class TrainSensorTest {
     @Test
     public void setSpeedLimitRelativeTooLowAlarm() {
         when(mockTC.getReferenceSpeed()).thenReturn(150);
-        when(mockTC.setAlarmState(true)).thenReturn(void);
-        sensor.setSpeedLimit(50);
+        doNothing().when(mockTC).setAlarmState(true);
+        sensor.overrideSpeedLimit(50);
         verify(mockTC, times(1)).setAlarmState(true);
     }
 
